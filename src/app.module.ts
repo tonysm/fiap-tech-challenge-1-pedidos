3f9abc4a50dba0ce7e2ff9798c5de2b550e7df2f
Tony Messias <tonysm@hey.com>
2023-06-09 00:30:15 -0300

    Configure database with typeORM and setup pedido entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,33 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PedidosModule } from './pedidos/pedidos.module';
 import { ProdutosModule } from './produtos/produtos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PedidosModule, ProdutosModule],
+  imports: [
+    ConfigModule.forRoot(),
+    PedidosModule,
+    ProdutosModule,
+    TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        useFactory(configService: ConfigService) {
+            return {
+                type: 'mysql',
+                host: configService.get<string>('DB_HOST'),
+                port: configService.get<number>('DB_PORT'),
+                username: configService.get<string>('DB_USERNAME'),
+                password: configService.get<string>('DB_PASSWORD'),
+                database: configService.get<string>('DB_DATABASE'),
+                entities: [__dirname + '/**/*.entity{.ts,.js}'],
+                synchronize: true,
+            }
+        },
+        inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/pedidos/entities/pedido.entity.ts
+++ src/pedidos/entities/pedido.entity.ts
@@ -1,6 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 export class Pedido {
   @ApiProperty({ example: 1, description: 'O identificador do Pedido' })
+  @PrimaryGeneratedColumn()
   id: number;
 }
